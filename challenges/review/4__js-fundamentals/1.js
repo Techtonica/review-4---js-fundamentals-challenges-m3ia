4ba850714ff35c2d4dcc2594fbe4b6bee33cfe13
Meia Natividad <meianatividad@gmail.com>
2021-02-05 22:26:40 -0800

    remove extra space

--- challenges/review/4__js-fundamentals/1.js
+++ challenges/review/4__js-fundamentals/1.js
@@ -1,5 +1,4 @@
 // Write a function below that accepts a number as the only argument.	
-
 // The function should determine whether the number is divisible by 3,	
 // and return either true or false.
 // Choose names that make the code easy to understand.
