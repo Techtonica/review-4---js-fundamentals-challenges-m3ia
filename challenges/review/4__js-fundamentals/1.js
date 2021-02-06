039a0298ff38cf07181e417896a73716cc11983e
Meia Natividad <meianatividad@gmail.com>
2021-02-05 20:37:03 -0800

    add initial solution

--- challenges/review/4__js-fundamentals/1.js
+++ challenges/review/4__js-fundamentals/1.js
@@ -1,6 +1,21 @@
-// Write a function below that accepts a number as the only argument.
-// The function should determine whether the number is divisible by 3,
-// and return either true or false.
+th                                                                           // and return either true or false.
 // Choose names that make the code easy to understand.
 //
 // Note:  None of the review # problems have tests included, but feel free to write your own.
+
+
+// 1. write a function that takes a num
+// 2. determine if num is div by 3
+// 3. return boolean. 
+
+
+/* Meia's trial space */ 
+function divBy3 (num) {
+  return (num % 3 === 0);
+}
+
+// trial lines:
+// console.log(divBy3(3)); // true
+// let testCases = [3, -3, 10, 0, 33333333] // [true, true, false, true, true]
+// console.log(testCases.map(divBy3)); 
+
