38ab3ee5dbec4ab8e2310012130318b4c1a45eab
Meia Natividad <meianatividad@gmail.com>
2021-02-05 22:25:44 -0800

    add removed part of prompt--done by error

--- challenges/review/4__js-fundamentals/1.js
+++ challenges/review/4__js-fundamentals/1.js
@@ -1,4 +1,7 @@
-th                                                                           // and return either true or false.
+// Write a function below that accepts a number as the only argument.	
+
+// The function should determine whether the number is divisible by 3,	
+// and return either true or false.
 // Choose names that make the code easy to understand.
 //
 // Note:  None of the review # problems have tests included, but feel free to write your own.
